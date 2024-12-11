094bc21281eeb42b2c663bfe7adaa1c3234bd3a5
Fabio Santos <fabiotrabmat@gmail.com>
2024-12-11 15:34:00 -0300

    feat: adicionando requisição com o redux/query

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,15 @@ import { BrowserRouter } from "react-router"
 import { GlobalStyle } from "./styles"
 import MyRoutes from "./routes"
 import Footer from "./components/Footer"
-import { useEffect, useState } from "react"
-import { RestaurantType } from "./types/models"
+import { useGetRestaurantsQuery } from "./services/api"
 
 export default function App() {
-	const [restaurants, setRestaurants] = useState<RestaurantType[]>()
-
-	useEffect(() => {
-		fetch("https://fake-api-tau.vercel.app/api/efood/restaurantes")
-			.then(response => response.json())
-			.then(data => setRestaurants(data))
-	}, [])
+	const { data: restaurants } = useGetRestaurantsQuery()
 
 	return (
 		<BrowserRouter>
 			<GlobalStyle />
-			{restaurants ? <MyRoutes restaurants={restaurants} /> : <h1>Carregando restaurantes...</h1>}
+			{!!restaurants ? <MyRoutes restaurants={restaurants} /> : <h1>Carregando restaurantes...</h1>}
 			<Footer />
 		</BrowserRouter>
 	)
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 import cartReducer from "./reducers/cartSlice"
+import api from "./../services/api"
 
 const store = configureStore({
 	reducer: {
 		cart: cartReducer,
+		[api.reducerPath]: api.reducer,
 	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware),
 })
 
 export default store
