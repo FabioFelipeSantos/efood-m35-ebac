248cb4988309a8f9a81860e6f200210418251e41
Fabio Santos <fabiotrabmat@gmail.com>
2024-12-16 13:29:58 -0300

    feat: adicionando spinner na página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router"
 
 import Footer from "./components/Footer"
-import { GlobalStyle } from "./styles"
+import { colors, GlobalStyle } from "./styles"
 
 import MyRoutes from "./routes"
 import { useGetRestaurantsQuery } from "./services/api"
+import { PropagateLoader } from "react-spinners"
 
 export default function App() {
 	const { data: restaurants } = useGetRestaurantsQuery()
@@ -12,7 +13,20 @@ export default function App() {
 	return (
 		<BrowserRouter>
 			<GlobalStyle />
-			{!!restaurants ? <MyRoutes restaurants={restaurants} /> : <h1>Carregando restaurantes...</h1>}
+			{!!restaurants ? (
+				<MyRoutes restaurants={restaurants} />
+			) : (
+				<div
+					className="container"
+					style={{
+						height: "100vh",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}>
+					<PropagateLoader size={28} speedMultiplier={0.8} color={colors.secondaryLight} />
+				</div>
+			)}
 			<Footer />
 		</BrowserRouter>
 	)
